76aac15836126fd4439bd5de3d0da3752ffd15d4
Alcazardavid <david.alcazar01@unach.mx>
2025-11-06 11:52:52 -0600

    vista reporte con datos simulados listo

--- frontend-angular/src/app/app.routes.ts
+++ frontend-angular/src/app/app.routes.ts
@@ -3,6 +3,8 @@
 import { Routes } from '@angular/router';
 import { Dashboard } from './dashboard/dashboard';
 import { Produccion } from './pages/produccion/produccion'; //agregamos el componente inventarion 
+import { Ventas } from './pages/ventas/ventas';
+import { Reportes } from './pages/reportes/reportes';
 
 export const routes: Routes = [
     { 
@@ -15,7 +17,16 @@ export const routes: Routes = [
         component: Dashboard 
     },
     {
-        path: 'produccion', component: Produccion
+        path: 'produccion', 
+        component: Produccion
+    },
+    {  
+        path: 'ventas', 
+        component: Ventas 
+    },
+    {
+        path: 'reportes', 
+        component: Reportes
     },
 
 ];
--- frontend-angular/src/app/pages/ventas/ventas.ts
+++ frontend-angular/src/app/pages/ventas/ventas.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-ventas',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './ventas.html',
   styleUrl: './ventas.scss',
 })
 export class Ventas {
+  // --- 1. Variable para guardar el tab activo ---
+  activeTab: string = 'dia'; // 'dia', 'semana', 'mes', 'año'
 
+  // --- 2. Datos simulados ---
+  totalVentas: string = '$4,500';
+
+  // --- 3. Método para cambiar el tab ---
+  seleccionarTab(tab: string): void {
+    this.activeTab = tab;
+    // Aquí, en el futuro, cargarías los datos para ese tab
+    // Ej: this.cargarVentasDelDia();
+  }
 }
